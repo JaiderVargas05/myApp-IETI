5997f13d59259d8b1da4e07254b7386fa4952357
JVARGAS <jaider.vargas-n@mail.escuelaing.edu.co>
2025-08-28 23:44:09 -0500

    Initial commit

---
+++ src/main/java/escuelaing/edu/co/myApp/controller/health/HealthController.java
@@ -0,0 +1,14 @@
+package escuelaing.edu.co.myApp.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping
+    public String checkApi(){
+        return "<h1>The API is working ok!</h1>";
+    }
+}
